2577e98d7e15c4dd5a49486c5b6eb24a2bd55539
akorbutovski <akorbutovski@elinext.com>
2023-09-18 12:47:14 +0300

    feat: useNavigate hook

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,20 @@
+import {Navigate, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 function NotFound() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        // navigate('/');
+        setTimeout(() => {
+            navigate(-1);
+        }, 1000)
+    }, []);
     return (
         <>
             <h2>
                 404 Not Found !
             </h2>
+            {/*<Navigate to='/'/>*/}
         </>
     );
 }
